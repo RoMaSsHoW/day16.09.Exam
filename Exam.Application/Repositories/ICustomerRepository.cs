a56b373d8f9181dd1791c25947d31ea9e1bab6be
RoMaSsHoW <romasakh27@gmail.com>
2025-09-16 18:59:18 +0500

    Создал интерфейс для репозитория Customer и создал его пустую реализацию

---
+++ Exam.Application/Repositories/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Exam.Domain.Entities;
+
+namespace Exam.Application.Repositories
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer>? FindByIdAsync(int id);
+        Task<IEnumerable<Customer>> FindAllAsync();
+        Task<int> CreateAsync(Customer customer);
+        Task<int> UpdateAsync(Customer customer);
+        Task<int> DeleteAsync(int id);
+    }
+}
---
+++ Exam.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,41 @@
+﻿using Exam.Application.Repositories;
+using Exam.Domain.Entities;
+using System.Data;
+
+namespace Exam.Infrastructure.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public CustomerRepository(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public Task<Customer>? FindByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Customer>> FindAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CreateAsync(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
