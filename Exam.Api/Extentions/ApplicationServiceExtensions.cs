bd7957e7e771ab00a25c2b0fcdf9a479fe072a14
RoMaSsHoW <romasakh27@gmail.com>
2025-09-16 18:25:56 +0500

    сделал базовые настройки

---
+++ Exam.Api/Extentions/ApplicationServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace Exam.Api.Extentions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(
+           this IServiceCollection services,
+           IConfiguration configuration)
+        {
+            //ConfigureRepositories(services);
+
+            ConfigureDatabase(services, configuration);
+
+            return services;
+        }
+
+        private static void ConfigureDatabase(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IDbConnection>(provider =>
+            {
+                return new NpgsqlConnection(configuration.GetConnectionString("PostgresqlDbConnection"));
+            });
+        }
+    }
+}
