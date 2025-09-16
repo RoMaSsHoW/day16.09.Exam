185bd45a3d069e8c570eb2f523b1c2984dae79ef
RoMaSsHoW <romasakh27@gmail.com>
2025-09-16 19:41:32 +0500

    написал интерфейсы репозиториев для Transaction Account

---
+++ Exam.Application/Repositories/IAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using Exam.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Application.Repositories
+{
+    public interface IAccountRepository
+    {
+        Task<Account>? FindByIdAsync(int id);
+        Task<IEnumerable<Account>> FindAllAsync();
+        Task<int> CreateAsync(Account account);
+        Task<int> UpdateAsync(Account account);
+        Task<int> DeleteAsync(int id);
+    }
+}
---
+++ Exam.Application/Repositories/ITransactionRepository.cs
@@ -0,0 +1,25 @@
+﻿using Exam.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Application.Repositories
+{
+    public interface ITransactionRepository
+    {
+        Task<Transaction>? FindByIdAsync(int id);
+        Task<IEnumerable<Transaction>> FindAllAsync();
+        Task<IEnumerable<Transaction>> SearchAsync(
+            int? customer_id, 
+            DateTime? date_from, 
+            DateTime? date_to, 
+            decimal? min_amount, 
+            decimal? max_amount, 
+            string? status);
+        Task<int> CreateAsync(Transaction transaction);
+        Task<int> UpdateAsync(Transaction transaction);
+        Task<int> DeleteAsync(int id);
+    }
+}
