8a3c72974e66f2f6d00ccf18509d19508c653194
RoMaSsHoW <romasakh27@gmail.com>
2025-09-16 19:29:43 +0500

    Заполнил модель Account

--- Exam.Domain/Entities/Account.cs
+++ Exam.Domain/Entities/Account.cs
@@ -1,13 +1,38 @@
 ﻿using Exam.Domain.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Exam.Domain.Entities
 {
     public class Account : Entity
     {
+        public Account() { }
+        public Account(
+            int customerId,
+            decimal balance,
+            string currency)
+        {
+            CustomerId = customerId;
+            Balance = balance;
+            Currency = currency.ToUpper();
+            AccountNumber = GenerateAccauntNumber();
+        }
+
+        public int CustomerId { get; set; }
+        public string AccountNumber { get; set; } = string.Empty;
+        public decimal Balance { get; set; }
+        public string Currency { get; set; } = string.Empty;
+
+        private string GenerateAccauntNumber()
+        {
+            var accNumber = new StringBuilder("UZ");
+            var rand = new Random();
+            for (var i = 0; i < 10; i++)
+            {
+                var number = rand.Next(9);
+                accNumber.Append(number);
+            }
+
+            return string.Empty;
+        }
     }
 }
