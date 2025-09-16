b92abb5dbc95294497a92d5a0a58f9a7fb4c3d76
RoMaSsHoW <romasakh27@gmail.com>
2025-09-16 19:33:41 +0500

    заполнил модель Transaction

--- Exam.Domain/Entities/Transaction.cs
+++ Exam.Domain/Entities/Transaction.cs
@@ -1,13 +1,12 @@
 ﻿using Exam.Domain.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Exam.Domain.Entities
 {
     public class Transaction : Entity
     {
+        public int FromAccountId { get; set; }
+        public int ToAccountId { get; set; }
+        public decimal Amount { get; set; }
+        public string Status { get; set; } = string.Empty;
     }
 }
