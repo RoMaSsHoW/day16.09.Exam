2c501c55552dc9b5dfd59f6d741976b5c0ea72fc
RoMaSsHoW <romasakh27@gmail.com>
2025-09-16 20:11:36 +0500

    Создал пустую реализацию репозиториев

---
+++ Exam.Infrastructure/Repositories/AccountRepository.cs
@@ -0,0 +1,41 @@
+﻿using Exam.Application.Repositories;
+using Exam.Domain.Entities;
+using System.Data;
+
+namespace Exam.Infrastructure.Repositories
+{
+    internal class AccountRepository : IAccountRepository
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public AccountRepository(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public Task<Account>? FindByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Account>> FindAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CreateAsync(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Exam.Infrastructure/Repositories/TransactionRepository.cs
@@ -0,0 +1,52 @@
+﻿using Exam.Application.Repositories;
+using Exam.Domain.Entities;
+using System.Data;
+
+namespace Exam.Infrastructure.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public TransactionRepository(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public Task<Transaction>? FindByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Transaction>> FindAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Transaction>> SearchAsync(
+            int? customer_id,
+            DateTime? date_from,
+            DateTime? date_to,
+            decimal? min_amount,
+            decimal? max_amount,
+            string? status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CreateAsync(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
